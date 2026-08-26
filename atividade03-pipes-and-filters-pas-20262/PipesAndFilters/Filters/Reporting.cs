bb76a6710cd66a0bc280e943268c68d2101243cf
mourayasmin <yasmin.moura@discente.ufg.br>
2026-08-26 09:16:40 -0300

    fix: ajuste de aparência na saída esperada

--- atividade03-pipes-and-filters-pas-20262/PipesAndFilters/Filters/Reporting.cs
+++ atividade03-pipes-and-filters-pas-20262/PipesAndFilters/Filters/Reporting.cs
@@ -8,7 +8,9 @@ public class Reporting : IFilter<ResumoVendas, string>
 {
     public string Executar(ResumoVendas resumoVendas)
     {
-        string report = "Vendas válidas: " + resumoVendas.VendasValidas + "\n Produtos vendidos: " + resumoVendas.TotalProdutos + "\n Valor total: " + resumoVendas.ValorTotal.ToString("C2", new CultureInfo("pt-BR"));
-        return report;
+        string headline = "========================================\n RELATÓRIO DE VENDAS\n========================================";
+        string report = "\n Vendas válidas: " + resumoVendas.VendasValidas + "\n Produtos vendidos: " + resumoVendas.TotalProdutos + "\n Valor total: " + resumoVendas.ValorTotal.ToString("C2", new CultureInfo("pt-BR"));
+        string endline = "\n========================================";
+        return headline + report + endline;
     }
 }
